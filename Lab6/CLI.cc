b284d0ce243a39dbc0740a4f0c6642c7f41885a8
KailinLi <likailinhust@gmail.com>
2018-02-18 21:47:49 +0800

    tmp

--- Lab6/CLI.cc
+++ Lab6/CLI.cc
@@ -4,8 +4,8 @@ int main (int argc, char *argv[]) {
     // printf("%ld\n", sizeof(i_node));
     // i_bitmap[0] = 9;
     // printf("%d\n", i_bitmap[0]);
-    openFp();
-    printf("%d %d\n", i_bitmap[0], b_bitmap[1]);
+    // openFp();
+    // printf("%d %d\n", i_bitmap[0], b_bitmap[1]);
     // i_bitmap[0] = 9;
     // b_bitmap[1] = 10;
     // writeThrough();
